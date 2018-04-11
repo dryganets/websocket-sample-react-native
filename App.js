b3151e24a241ddadfe46a7eb7c9dd750af984399
Sergei Dryganets <sergeyd@microsoft.com>
2018-04-11 12:37:03 -0700

    WebSocket usage added

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const Tag = 'WS';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { connected: false };
+    window.navigator.userAgent = 'react-native';
+  }
+
+  _connectSocket = () => {
+    this.ws = new WebSocket('ws://104.236.168.186:3000');
+    this.ws.onerror = (e) => {
+      console.log(Tag, 'Error', e.message);
+    };
+
+    this.ws.onopen = (ws, event) => {
+      console.log(Tag, "Socket open");
+    };
+
+    this.ws.onclose = () => {
+      console.log(Tag, "Socket closed");
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button 
+          title = 'Connect'
+          onPress = { this._connectSocket } />
       </View>
     );
   }
